0a08de4755ff1550b218578ab5d6acabdc67d083
devanjana401 <anjanasasiaduvad@gmail.com>
2026-05-08 23:40:53 +0530

    added

--- edubackend/asgi.py
+++ edubackend/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edubackend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
